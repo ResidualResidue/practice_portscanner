fb8ae2592f4e99d54e5b3cad5cecb1ffdc75264c
ResidualResidue <michaelfoley1999@gmail.com>
2024-09-10 21:25:13 -0400

    PoC port scan my VMs first 100 ports

--- scanner.py
+++ scanner.py
@@ -1,1 +1,21 @@
 import socket
+
+verbose=False
+
+host='192.168.57.1'
+ports=range(0, 100)
+
+
+
+scanner = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+for port in ports:
+    connection_info = (host,port)
+    try:
+        scanner.connect(connection_info)
+        print(f'Port {port} open')
+    except:
+        if(verbose):
+            print(f'Port {port} closed')
+
+

f4c9bbf33d6568286701d9e09c48a6490a5d1a03
ResidualResidue <michaelfoley1999@gmail.com>
2024-09-11 12:31:09 -0400

    parse out port ranges for errors and get range as a list

--- scanner.py
+++ scanner.py
@@ -2,6 +2,37 @@ import socket
 import argparse
 import sys
 
+#
+
+def validate_port_range_syntax(port_ranges):
+    for i in range(0, len(port_ranges)-2):
+        if((',' not in port_ranges[i]) and (',' not in port_ranges[i+1]) and (',' not in port_ranges[i+2])):
+            print('invalid port range')
+            sys.exit()
+    for port_range in port_ranges:
+        if(port_range[-1] == ',' or port_range[0] == ','):
+            print('invalid port range')
+            sys.exit()
+
+def validate_port_range_low_to_high(low_port, high_port):
+    if(int(low_port) >= int(high_port)):
+        print("In a port range (i.e. 5-10) the first port should be strictly lower than the last.")
+        print(f"ERROR: {low_port} is greater than or equal to {high_port} in range '{low_port}-{high_port}'")
+
+def get_ports_from_range(low_port, high_port):
+    return list(range(int(low_port), int(high_port)+1))
+
+def parse_ports(ports: str) -> list:
+    port_ranges = ports.split('-')
+    validate_port_range_syntax(port_ranges)
+    for i in range(0, len(port_ranges)-1):
+        low_port = port_ranges[i].split(',')[-1]
+        high_port = port_ranges[i+1].split(',')[0]
+        validate_port_range_low_to_high(low_port, high_port)
+        ports_from_range = get_ports_from_range(low_port, high_port)
+        print(ports_from_range)
+
+
 parser = argparse.ArgumentParser(
     prog="practice-scanner",
     description="Small little scanner I made to practice the socket library we cover in TCMs PNPT",
@@ -10,10 +41,10 @@ parser = argparse.ArgumentParser(
 
 parser.add_argument('host')
 parser.add_argument('-v', '--verbose', action='store_true')
-parser.add_argument('-p', '--ports', action='store', help='Arguments can be in the formats:\nstart-end port\nport (single)\nport1,port2,...,portn')
+parser.add_argument('-p', '--ports', action='store', help='Arguments can be in the formats:\nstart-end port\nport (single)\nport1,port2,...,portn', required=True)
 
 args = parser.parse_args()
-
+parse_ports(args.ports)
 verbose=args.verbose
 host=args.host
 
@@ -25,15 +56,15 @@ ports=range(0, 100)
 
 
 
-scanner = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# scanner = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-for port in ports:
-    connection_info = (host,port)
-    try:
-        scanner.connect(connection_info)
-        print(f'Port {port} open')
-    except:
-        if(verbose):
-            print(f'Port {port} closed')
+# for port in ports:
+#     connection_info = (host,port)
+#     try:
+#         scanner.connect(connection_info)
+#         print(f'Port {port} open')
+#     except:
+#         if(verbose):
+#             print(f'Port {port} closed')
 
 

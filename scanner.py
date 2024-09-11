2dd989095678a71177a9b43da7876885c5b636b2
ResidualResidue <michaelfoley1999@gmail.com>
2024-09-10 21:42:38 -0400

    Added first PoC for argsparsing

--- scanner.py
+++ scanner.py
@@ -1,6 +1,18 @@
 import socket
+import argparse
+import sys
 
-verbose=False
+parser = argparse.ArgumentParser(
+    prog="practice-scanner",
+    description="Small little scanner I made to practice the socket library we cover in TCMs PNPT",
+    epilog="To whomever may read this: You are beautiful and belong here in the world <3"
+)
+
+parser.add_argument('-v', '--verbose', action='store_true')
+
+args = parser.parse_args()
+
+verbose=args.verbose
 
 host='192.168.57.1'
 ports=range(0, 100)
